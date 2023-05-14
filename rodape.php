b471bde9b4d6b06ca8ac65cba5b01553722f6b9c
Rodrigo Camurça Vera <131033913+rcvdigo@users.noreply.github.com>
2023-05-13 22:47:23 -0300

    Delete

--- rodape.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo "<footer>";
-    echo"<p>Acessado por ". $_SERVER['SERVER_NAME'] ." em". date('d/m/Y') ."</p>";
-    echo"<p>Desenvolvido por Rodrigo Camurça Vera &copy; 2023</p>";
-    echo"</footer>";
-    // $banco->close();
-    $banco = null;
-?>
